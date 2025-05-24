1e5e10dbf8b1efdee1afb6eecf6e0655b5a8c0f5
unknown <katotodan@gmail.com>
2025-05-24 15:00:55 +0300

    Adding default port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 const process = require('process')
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(cors({
   origin: process.env.FRONTEND_URL
